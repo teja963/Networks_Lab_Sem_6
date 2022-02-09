aa4dedb1d82a77c5b9a18dc286855cd335d66455
teja963 <panasateja123@gmail.com>
2022-02-09 23:14:19 +0530

    TCP client and server established

--- assg_3/server.c
+++ assg_3/server.c
@@ -6,80 +6,74 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #define MAX_SIZE 1024
-void black () {
-  printf("\033[1;30m");     //  Escape is: \033
-}                           // Color Code is: [0;30m
-
-void red() {
-  printf("\033[1;31m");
-}
-
-void green() {
-  printf("\033[1;32m");
-}
-
-void yellow() {
-  printf("\033[1;33m");
-}
-
-void blue() {
-  printf("\033[1;34m");
-}
-
-void purple() {
-  printf("\033[1;35m");
-}
-
-void cyan() {
-  printf("\033[1;36m");
-}
-
-void reset () {
-  printf("\033[0m");           //Resets the text to defaults
-}
 int main(){
 	  char* ip_addr = "127.0.0.1";
 	  int port = 5000;
-	  int sock_server, sock_client;
+	  int server_sock, client_sock;
 	  struct sockaddr_in server_addr, client_addr;
 	  socklen_t  addr_size;
 	  int n;
 	  char buffer[MAX_SIZE];
 	  /*   socket connection protocol(domain, type, protocol)   */
-	  sock_server = socket(AF_INET, SOCK_STREAM, 0);
-	  if(sock_server < 0)
+	  server_sock = socket(AF_INET, SOCK_STREAM, 0);
+	  if(server_sock < 0)
 	  {
-	     red();
-	     printf("[-]Socket Connection is not established!!\n");
-	     reset();
+	                                                                                                                  
+	     perror("[-]Socket Connection is not established!!\n");
+	                                                                                                                  
 	     exit(1);
 	  }
 	  else
 	  {
-	     green();
+	                                                                                                                  
 	     printf("[+]Succesfully Established Socket connection!!\n");
-	     reset();
+	                                                                                                                  
 	  }
 	  /* Intializing server_addr by memset takes pointers thats here server_addr is struct type*/
-	  memset(&server_addr, 0, sizeof(server_addr));
+	  memset(&server_addr, '\0', sizeof(server_addr));
 	  server_addr.sin_family = AF_INET;
 	  server_addr.sin_port   = port;
 	  server_addr.sin_addr.s_addr = inet_addr(ip_addr);
 	  /*  Binding protocol (sock_connection, struct sockaddr* server_addr, size)   */
-	  n = bind(sock_server, (struct sockaddr*)&server_addr, sizeof(server_addr));
-	  if(n > 0)
+	  n = bind(server_sock, (struct sockaddr*)&server_addr, sizeof(server_addr));
+	  if(n < 0)
 	  {
-	     red();
-	     printf("[-]Binding Connection is not established!!\n");
-	     reset();
+	                                                                                                                   
+	     perror("[-]Binding Connection is not established!!\n");
+	                                                                                                                   
 	     exit(1);
 	  }
 	  else
 	  {
-	     green();
-	     printf("[+]Succesfully Established Binding connection!!\n");
-	     reset();
+	                                                                                                                   
+	     printf("[+]Succesfully Bind with port num: %d connection!!\n",port);
+	                                                                                                                  
 	  }
+	  
+	  /* After connection established we need to listen */
+	  listen(server_sock, 5);
+	                                                                                                                  
+	  printf("Listening...........\n");
+	                                                                                                                  
+	  while(1)
+	  {
+	    addr_size = sizeof(client_addr);                                                                               
+	    client_sock = accept(server_sock, (struct sockaddr*)&client_addr, &addr_size);                                   
+	    printf("[+]Client Connected!!\n\n");
+	                                                                                                                   
+	    bzero(buffer, 1024);
+	    recv(client_sock, buffer, sizeof(buffer), 0);
+	    printf("Client: %s", buffer);
+	                                                                                                                   
+	    bzero(buffer, 1024);                                                                                                
+	    strcpy(buffer, "HI, THIS IS SERVER\n");
+	    printf("Server: %s\n", buffer);
+	    send(client_sock, buffer, sizeof(buffer), 0);
+	    
+	    close(client_sock);                                                     
+	    printf("[-]Client disconnected\n");                                                                           
+	  }
+	  
 	  return 0;
   }
 
